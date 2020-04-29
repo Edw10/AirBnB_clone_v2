602a8fb67ae76f4c3874d500559729d894803dc9
edw10 <edward-1094@hotmail.com>
2020-04-29 20:24:26 +0000

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,12 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 @app.teardown_appcontext
-def teardown_appcontext(machete):
+def teardown_appcontext(exc):
     storage.close()
 
 if __name__ == "__main__":
