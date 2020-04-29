35ad0223587664f532bee689bf63506e47a4a20b
edw10 <edward-1094@hotmail.com>
2020-04-29 20:58:20 +0000

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,8 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exc):
@@ -16,6 +16,6 @@ def teardown_appcontext(exc):
     storage.close()
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv("HBNB_API_HOST", default = "0.0.0.0")
-    HBNB_API_PORT = getenv("HBNB_API_PORT", default = 5000)
-    app.run(host=HBNB_API_HOST,port= HBNB_API_PORT, threaded=True)
+    HBNB_API_HOST = getenv("HBNB_API_HOST", default="0.0.0.0")
+    HBNB_API_PORT = getenv("HBNB_API_PORT", default=5000)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
