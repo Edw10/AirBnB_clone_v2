3e2d7ff9797c7552f35e648e49f29c8af6aac278
edw10 <edward-1094@hotmail.com>
2020-04-30 03:47:01 +0000

    add

--- test_get_count.py
+++ test_get_count.py
@@ -3,11 +3,15 @@
 """
 from models import storage
 from models.state import State
+from flask import jsonify
 
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
 
+listo = []
 print(storage.all(State).values())
-
+for obj in storage.all(State).values():
+    listo.append(obj.to_dict())
+print(listo)
 first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
